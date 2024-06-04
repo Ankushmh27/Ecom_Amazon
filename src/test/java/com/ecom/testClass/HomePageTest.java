a9a8d699b8d2c636994ad86209e957b5ff94aacc
Ankush <ankushmhala2@gmail.com>
2024-06-05 00:11:38 +0530

    dummy comments

--- src/test/java/com/ecom/testClass/HomePageTest.java
+++ src/test/java/com/ecom/testClass/HomePageTest.java
@@ -5,11 +5,17 @@ import com.ecom.baseClass.SetUpBrowser;
 import com.ecom.pageObject.HomePage;
 public class HomePageTest extends SetUpBrowser
 {
-	HomePage homePage;
-	@Test()
-	public void VerifySerchBox() throws InterruptedException 
-	{   
+	HomePage homePage = new HomePage(driver);
+//	@Test(priority = 1)
+//	public void VerifySerchBox() throws InterruptedException 
+//	{   
+//		
+//		homePage.VerifySearchBox();
+//	}
+	@Test(priority = 0)
+	public void VerifyRibben() throws InterruptedException 
+	{ 
 		homePage = new HomePage(driver);
-		homePage.VerifySearchBox();
+		homePage.VerifyAllRibbenElement();
 	}
 }
